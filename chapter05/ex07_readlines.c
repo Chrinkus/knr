35cd40955ffeabb08d4004ffd69bec8691a9a663
Chrinkus <seeschickrun@gmail.com>
2018-10-06 10:36:02 -0500

    Picking up work again. Pushing to pull

---
+++ chapter05/ex07_readlines.c
@@ -0,0 +1,97 @@
+/*
+ * Exercise 5-7
+ *
+ * Rewrite readlines to store lines in an array supplie by main, rather than
+ * calling alloc to maintain storage. How much faster is the program?
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINES    5000                /* max lines to be sorted */
+
+char *lineptr[MAXLINES];                /* pointers to text lines */
+
+int  readlines(char *lineptr[], int nlines);
+void writelines(char *lineptr[], int nlines);
+
+void qsort(char *lineptr[], int left, int right);
+
+/* sort input lines */
+int main()
+{
+    int nlines;                         /* number of input lines read */
+    if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+        qsort(lineptr, 0, nlines - 1);
+        writelines(lineptr, nlines);
+        return 0;
+    } else {
+        printf("error: input too big to sort\n");
+        return 1;
+    }
+}
+
+#define MAXLEN  1000                    /* max length of any input line */
+int my_getline(char *, int);
+char *alloc(int);
+
+/* readlines: read input lines */
+int readlines(char *lineptr[], int maxlines)
+{
+    int len, nlines;
+    char *p, line[MAXLEN];
+
+    nlines = 0;
+    while ((len = my_getline(line, MAXLEN)) > 0)
+        if (nlines >= maxlines || (p = alloc(len)) == NULL)
+            return 1;
+        else {
+            line[len-1] = '\0';         /* delete newline */
+            strcpy(p, line);
+            lineptr[nlines++] = p;
+        }
+    return nlines;
+}
+
+/* writelines: write output lines */
+void writelines(char *lineptr[], int nlines)
+{
+    while (nlines-- > 0)
+        printf("%s\n", *lineptr++);
+}
+
+/* my_getline: get a line from input */
+int my_getline(char *s, int lim)
+{
+    char *p = s;
+    int c;
+
+    while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+        *s++ = c;
+    if (c == '\n')
+        *s++ = c;
+    *s = '\0';
+    return s - p;
+}
+
+#define ALLOCSIZE   10000               /* size of available space */
+
+static char allocbuf[ALLOCSIZE];        /* storage for alloc */
+static char *allocp = allocbuf;         /* next free position */
+
+/* alloc: return pointer to n characters */
+char *alloc(int n)
+{
+    if (allocbuf + ALLOCSIZE - allocp >= n) {       /* it fits */
+        allocp += n;
+        return allocp - n;              /* old p */
+    } else          /* not enough room */
+        return 0;
+}
+
+/*afree: free storage pointed to by p */
+void afree(char *p)
+{
+    if (p >= allocbuf && p < allocbuf + ALLOCSIZE)
+        allocp = p;
+}
