ab9a8b1f88b3f412a103e3c199a517d12899016e
Chrinkus <seeschickrun@gmail.com>
2018-10-21 22:43:24 -0500

    Pushing minimal progress

--- chapter05/ex07_readlines.c
+++ chapter05/ex07_readlines.c
@@ -95,3 +95,31 @@ void afree(char *p)
     if (p >= allocbuf && p < allocbuf + ALLOCSIZE)
         allocp = p;
 }
+
+/* qsort: sort v[left]...v[right] into increasing order */
+void qsort(char *v[], int left, int right)
+{
+    int i, last;
+    void swap(char *v[], int i, int j);
+
+    if (left >= right)                  /* do nothing if array contains */
+        return;                         /* fewer than two elements */
+    swap(v, left, (left + right) / 2);
+    last = left;
+    for (i = left + 1; i <= right; i++)
+        if (strcmp(v[i], v[left]) < 0)
+            swap(v, ++last, i);
+    swap(v, left, last);
+    qsort(v, left, last - 1);
+    qsort(v, last + 1, right);
+}
+
+/* swap: interchange v[i] and v[j] */
+void swap(char *v[], int i, int j)
+{
+    char *temp;
+
+    temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+}
