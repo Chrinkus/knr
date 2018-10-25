f868bca6426eef6e4c67664ea27d4fb1e495511f
Chrinkus <seeschickrun@gmail.com>
2018-10-25 11:19:06 -0500

    Committing to merge ex07

--- chapter05/ex07_readlines.c
+++ chapter05/ex07_readlines.c
@@ -3,25 +3,36 @@
  *
  * Rewrite readlines to store lines in an array supplie by main, rather than
  * calling alloc to maintain storage. How much faster is the program?
+ *
+ * My understanding of the purpose of this exercise is to compare the speed
+ * of working with static/global memory and auto/local memory. To achieve a
+ * meaningful result I will need to work with larger input data than my usual
+ * handful of typed lines.
  */
 
 #include <stdio.h>
 #include <string.h>
 
 #define MAXLINES    5000                /* max lines to be sorted */
+#define STACKSIZE   10000               /* max size of artificial stack */
 
 char *lineptr[MAXLINES];                /* pointers to text lines */
 
-int  readlines(char *lineptr[], int nlines);
+int  readlines(char *lineptr[], int nlines, char *stack, char *stackp);
 void writelines(char *lineptr[], int nlines);
 
 void qsort(char *lineptr[], int left, int right);
 
+char *stackalloc(char *stack, char *stackp, int n);
+
 /* sort input lines */
 int main()
 {
+    char stackbuf[STACKSIZE];
+    char *stackp = stackbuf;
+
     int nlines;                         /* number of input lines read */
-    if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+    if ((nlines = readlines(lineptr, MAXLINES, stackbuf, stackp)) >= 0) {
         qsort(lineptr, 0, nlines - 1);
         writelines(lineptr, nlines);
         return 0;
@@ -36,19 +47,20 @@ int my_getline(char *, int);
 char *alloc(int);
 
 /* readlines: read input lines */
-int readlines(char *lineptr[], int maxlines)
+int readlines(char *lineptr[], int maxlines, char *stack, char *stackp)
 {
     int len, nlines;
     char *p, line[MAXLEN];
 
     nlines = 0;
     while ((len = my_getline(line, MAXLEN)) > 0)
-        if (nlines >= maxlines || (p = alloc(len)) == NULL)
-            return 1;
+        if (nlines >= maxlines || (p = stackalloc(stack, stackp, len)) == NULL)
+            return -1;
         else {
             line[len-1] = '\0';         /* delete newline */
             strcpy(p, line);
             lineptr[nlines++] = p;
+            stackp += len;
         }
     return nlines;
 }
@@ -74,24 +86,12 @@ int my_getline(char *s, int lim)
     return s - p;
 }
 
-#define ALLOCSIZE   10000               /* size of available space */
-
-static char allocbuf[ALLOCSIZE];        /* storage for alloc */
-static char *allocp = allocbuf;         /* next free position */
-
-/* alloc: return pointer to n characters */
-char *alloc(int n)
+/* stackalloc: return pointer to n characters in stack */
+char *stackalloc(char *stack, char *stackp, int n)
 {
-    if (allocbuf + ALLOCSIZE - allocp >= n) {       /* it fits */
-        allocp += n;
-        return allocp - n;              /* old p */
-    } else          /* not enough room */
+    if (stack + STACKSIZE - stackp >= n) {
+        stackp += n;
+        return stackp - n;
+    } else
         return 0;
-}
-
-/*afree: free storage pointed to by p */
-void afree(char *p)
-{
-    if (p >= allocbuf && p < allocbuf + ALLOCSIZE)
-        allocp = p;
 }
